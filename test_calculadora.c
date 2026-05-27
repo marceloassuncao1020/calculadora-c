2d8b73ad5d3b0113c333b11df2baa5d974958b29
marceloassuncao1020 <marceloassuncao1020@gmail.com>
2026-05-27 19:48:03 -0300

    Add tests for multiplicar function in test_calculadora.c

--- test_calculadora.c
+++ test_calculadora.c
@@ -1,1 +1,10 @@
-
+#include <stdio.h>
+#include <assert.h>
+#include "function_multiplicar.c"
+int main(){
+  assert(multiplicar(2, 0) == 2*0);
+  assert(multiplicar(3, 6) == 3*6);
+  assert(multiplicar(42, 64) == 42*64);
+  printf("Teste feito com execelência");
+return 0;
+}
